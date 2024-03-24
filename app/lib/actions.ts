945e699107ff7749e2269cd8fc2f2b8b6e6b67f7
Kenji Hashimoto <hashimoto.kenji.1102@gmail.com>
2024-03-24 20:33:38 +0900

    chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,10 @@
 'use server';
+import { signIn } from '@/auth';
 import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
-
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -103,4 +104,23 @@ export async function updateInvoice(
 }
 export async function deleteInvoice(id: string) {
   throw new Error('Failed to Delete Invoice');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
